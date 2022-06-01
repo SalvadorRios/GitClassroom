4cb69ff14232922dcd5d52f26584215d570d4cf6
SalvadorRios <salvador.rios@interx.us>
2022-06-01 10:17:26 -0500

    git frist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'hola mundo';
-  textContent = 'hola mundo app is running!';
+  title = 'App Component';
 }
